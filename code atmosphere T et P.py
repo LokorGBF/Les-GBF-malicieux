2bed26605b97e46afa339a4c18bff77d4b19de16
markoselic <marko082007@gmail.com>
2026-06-12 17:26:12 +0200

    Update code atmosphere T et P.py

--- code atmosphere T et P.py
+++ code atmosphere T et P.py
@@ -2,65 +2,65 @@ import numpy as np
 import matplotlib.pyplot as plt
 def AtmTetP() :
 
-G= 6.67e-11         # m/s²
-Mair = 29e-3      # kg/mol
-R = 8.314         # J/(K.mol)
-Mterre = 5.97e24
-RT= 6378e3
+    G= 6.67e-11         # m/s²
+    Mair = 29e-3      # kg/mol
+    R = 8.314         # J/(K.mol)
+    Mterre = 5.97e24
+    RT= 6378e3
 
-Tsol = 288        # K
-Psol = 1.013e5    # Pa
-
-
-# Gradient thermique ISA
-def kISA(z):
-    if 0 <= z < 11e3:
-        return -6.5e-3
-    elif z < 20e3:
-        return 0
-    elif z < 32e3:
-        return 1.0e-3
-    elif z < 47e3:
-        return 2.8e-3
-    elif z < 51e3:
-        return 0
-    elif z < 71e3:
-        return -2.8e-3
-    elif z <= 85e3:
-
-        return -2.0e-3
-    else:
-        return 0
-
-# Discrétisation de l'altitude
-N = 10000
-Z = np.linspace(0, 85e3, N)
-
-# Pas d'intégration
-dz = Z[1] - Z[0]
-
-# Tableaux de stockage
-T = np.zeros(N)
-P = np.zeros(N)
-
-# Conditions initiales
-T[0] = Tsol
-P[0] = Psol
+    Tsol = 288        # K
+    Psol = 1.013e5    # Pa
 
 
-# Méthode d'Euler
-for i in range(N - 1):
-    g = G*Mterre/(RT+Z[i])**2
-    dT = kISA(Z[i])
+    # Gradient thermique ISA
+    def kISA(z):
+        if 0 <= z < 11e3:
+            return -6.5e-3
+        elif z < 20e3:
+            return 0
+        elif z < 32e3:
+            return 1.0e-3
+        elif z < 47e3:
+            return 2.8e-3
+        elif z < 51e3:
+            return 0
+        elif z < 71e3:
+            return -2.8e-3
+        elif z <= 85e3:
 
-    dP = -(Mair * g / R) * P[i] / T[i]
+            return -2.0e-3
+        else:
+            return 0
 
-    T[i + 1] = T[i] + dT * dz
-    P[i + 1] = P[i] + dP * dz
+    # Discrétisation de l'altitude
+    N = 10000
+    Z = np.linspace(0, 85e3, N)
 
-# Modèle isotherme
-def PisoT(z, T0=288):
-    return Psol * np.exp(-Mair * g * z / (R * T0))
+    # Pas d'intégration
+    dz = Z[1] - Z[0]
+
+    # Tableaux de stockage
+    T = np.zeros(N)
+    P = np.zeros(N)
+
+    # Conditions initiales
+    T[0] = Tsol
+    P[0] = Psol
+
+
+    # Méthode d'Euler
+    for i in range(N - 1):
+        g = G*Mterre/(RT+Z[i])**2
+        dT = kISA(Z[i])
+
+        dP = -(Mair * g / R) * P[i] / T[i]
+
+        T[i + 1] = T[i] + dT * dz
+        P[i + 1] = P[i] + dP * dz
+
+    # Modèle isotherme
+    def PisoT(z, T0=288):
+        return Psol * np.exp(-Mair * g * z / (R * T0))
 
 
     return (T,P)
